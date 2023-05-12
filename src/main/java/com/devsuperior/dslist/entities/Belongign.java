d64f03b2c288899e0ef28e650723084acbb30092
Jose Augusto <jaleonimartins@hotmail.com>
2023-05-11 21:10:42 -0300

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belongign.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belongign {
+
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	private Integer position;
+	
+	public Belongign() {
+		
+	}
+
+	public Belongign(Game game, GameList list, Integer position) {
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belongign other = (Belongign) obj;
+		return Objects.equals(id, other.id);
+	}
+}
